ae551f21f05e0b03e226cca60447dbf497b4d516
anushakothamasu <anusha.kothamasu@hotmail.com>
2020-06-16 18:50:15 +1000

    My cucumber framework

---
+++ src/test/java/runners/MainRunner.java
@@ -0,0 +1,20 @@
+package resources.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features= {"src/test/java/resources/features/"},
+		glue = {"stepDefinitions"},
+		plugin= {"pretty",
+				"html:target/SystemTestReports/html"},
+				tags = {"@FullTestSuite"},
+				dryRun = false,
+				monochrome = true
+		)
+public class MainRunner {
+
+}
